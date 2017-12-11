5b3136178c923fb028bdd055c428af1f7f80b4f5
Levindo Neto <levindogtn@gmail.com>
2017-12-11 02:27:32 +0100

    Add open client button

--- sgui.py
+++ sgui.py
@@ -14,11 +14,17 @@ from PyQt5.QtCore import Qt
 
 
 class Server(QDialog):
+    ''' Fonts '''
+    INPUT_FONT = {"Type": "Arial", "Size": "20", "Style": "bold"}
+    LABEL_FONT = {"Type": "Arial", "Size": "20", "Style": "bold italic"}
+    TOOL_TIP = {"Type": "Arial", "Size": "8", "Style": "normal"}
+
+    ''' In order to have the icon on the taskbar '''
+    myappid = u'mycompany.myproduct.subproduct.version'
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
     def __init__(self, parent=None):
         PORT = 8888 # ToDo: Get from the current app
         super(Server, self).__init__(parent) # Init the gui server
-        myappid = u'mycompany.myproduct.subproduct.version' # In order to have the icon also in the task bar
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
 
         # Button for starting the server with a ballon help
         self.setWindowIcon(QIcon('icon.ico'))
@@ -28,7 +34,11 @@ class Server(QDialog):
         statusLabel.setWordWrap(True)
         portLabel = QLabel()
         portLabel.setWordWrap(True)
-        quitButton = QPushButton("Close the Server")
+        spaceLabel = QLabel()
+        spaceLabel.setWordWrap(True)
+        openClientButton = QPushButton("Open Client")
+        openClientButton.setAutoDefault(False)
+        quitButton = QPushButton("Close")
         quitButton.setAutoDefault(False)
 
         self.server = QLocalServer()
@@ -38,20 +48,28 @@ class Server(QDialog):
             self.close()
             return
 
-        statusLabel.setText("The server is running.\n " "It is running at the port ")
+        statusLabel.setText("The server has started running.\n " "It is running at the port ")
+        statusLabel.setAlignment(Qt.AlignCenter)
         portLabel.setText(str(PORT))
+        portLabel.setAlignment(Qt.AlignCenter)
+        spaceLabel.setText("\n\n")
+        spaceLabel.setAlignment(Qt.AlignCenter)
+        openClientButton.clicked.connect(self.openClient)
         quitButton.clicked.connect(self.close)
         self.server.newConnection.connect(self.startServer)
 
         buttonLayout = QHBoxLayout()
         buttonLayout.addStretch(1)
+        buttonLayout.addWidget(openClientButton)
         buttonLayout.addWidget(quitButton)
         buttonLayout.addStretch(1)
 
         mainLayout = QVBoxLayout()
+        mainLayout.setContentsMargins(0, 50, 0, 50)
         mainLayout.addWidget(statusLabel)
         mainLayout.addWidget(portLabel)
-        mainLayout.addLayout(buttonLayout)
+        mainLayout.addWidget(spaceLabel)
+        mainLayout.addLayout(buttonLayout) # Button for closing the server
         self.setLayout(mainLayout)
 
         self.setWindowTitle("Time Tagger | Web Application")
@@ -70,6 +88,8 @@ class Server(QDialog):
         clientConnection.flush()
         clientConnection.disconnectFromServer()
 
+    def openClient(self):
+        print("Open Client")
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
